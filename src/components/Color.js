e51b77a575199ab0acff84f0a4cf885ee6aa91ef
Leo Generali <leogen17@gmail.com>
2017-10-12 12:19:00 -0400

    Add icon to color hover

--- src/components/Color.js
+++ src/components/Color.js
@@ -14,10 +14,18 @@ class Color extends Component {
     });
   }
 
-  render() {
+  render() {    
     return (
       <div className={css(styles.card)} onMouseEnter={this.handleHover} onMouseLeave={this.handleHover}>
-        <div className={css(styles.color)} style={{backgroundColor: this.props.color}}></div>
+        <div className={css(styles.color)} style={{backgroundColor: this.props.color}}>
+          <svg 
+            className={css(styles.saveIcon)} 
+            viewBox="-2 -2 12 12"
+            style={{opacity: this.state.hovering ? '1' : '0'}}
+          >
+            <path d="M3 0v3h-2l3 3 3-3h-2v-3h-2zm-3 7v1h8v-1h-8z" />
+          </svg>
+        </div>
         <div className={css(styles.info)}>
           <p className={css(styles.text)} style={{color: this.props.color}}>{this.props.color}</p>
         </div>
@@ -46,7 +54,20 @@ const styles = StyleSheet.create({
   },
 
   color: {
-    height: '180px'
+    height: '180px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  saveIcon: {
+    height: '55px',
+    width: '55px',
+    padding: '10px',
+    borderRadius: '100%',
+    transition: card.transition,
+    fill: 'rgba(0, 0, 0, 0.75)',
+    backgroundColor: 'rgba(255, 255, 255, 0.20)'
   },
 
   info: {
